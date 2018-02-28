2ebde4beb8c614799ca5e043ec98e3851a31ac1a
Dariusz Skiciak <dariusz.skiciak@gmail.com>
2018-02-28 14:15:06 +0100

    Feat: - add Sing Out to headers

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,24 +1,41 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
 class Header extends Component {
+    static propTypes = {
+        authenticated: PropTypes.bool
+    };
+
+    static defaultProps = {
+        authenticated: false
+    };
 
     renderLinks = () => {
-        return [
-            <li className="nav-item" key={1}>
-                <Link className="nav-link" to="/signin">Sign In</Link>
-            </li>,
-            <li className="nav-item" key={2}>
-                <Link className="nav-link" to="/signup">Sign Up</Link>
+        if (this.props.authenticated) {
+            return <li className="nav-item" key={1}>
+                <Link className="nav-link" to="/signout">Sign Out</Link>
             </li>
-        ];
+        } else {
+            return <Fragment>
+                <li className="nav-item" key={1}>
+                    <Link className="nav-link" to="/signin">Sign In</Link>
+                </li>
+                <li className="nav-item" key={2}>
+                    <Link className="nav-link" to="/signup">Sign Up</Link>
+                </li>
+            </Fragment>
+        }
     };
 
 
     render() {
         return (
             <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <a className="navbar-brand" href="">Navbar</a>
@@ -44,4 +61,6 @@ class Header extends Component {
     }
 }
 
-export default Header;
+const mapStateToProps = state => ({authenticated: state.auth.authenticated});
+
+export default connect(mapStateToProps)(Header);
